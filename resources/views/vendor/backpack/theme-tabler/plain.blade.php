f7257e22f6a4db21d9a1264800682ced8157a40c
Rodolfo Gonzalez <rodogonzalez.cr@gmail.com>
2025-11-04 07:01:17 -0600

    updates

--- resources/views/vendor/backpack/theme-tabler/plain.blade.php
+++ resources/views/vendor/backpack/theme-tabler/plain.blade.php
@@ -28,7 +28,19 @@
 
         gtag('config', 'G-8MHG3Q37WW');
     </script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17701416634">
+    </script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'AW-17701416634');
+    </script>
 </head>
 
 <body id="app" class=" landing_page" bp-layout="plain">
