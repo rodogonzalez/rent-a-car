f3218fae7764e13e02268921ee96655576fa9a52
Rodolfo Gonzalez <rodogonzalez.cr@gmail.com>
2025-11-26 10:52:53 -0600

    update

--- resources/views/vendor/backpack/theme-tabler/default.blade.php
+++ resources/views/vendor/backpack/theme-tabler/default.blade.php
@@ -109,7 +109,7 @@
     @yield('after_scripts')
     @stack('after_scripts')
 
-    <footer>
+    <footer class="text-center">
         <p>INFOTURISTICA 2025</p>
         Infoturística solo realiza la reserva con Álamo, permitiéndole acceder a tarifas preferenciales y beneficios exclusivos. <b>No procesamos ningún cobro o cargo</b>; cualquier pago es gestionado directamente por Álamo.f
     </footer>
