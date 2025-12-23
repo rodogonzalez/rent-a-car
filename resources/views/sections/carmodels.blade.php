9a2e843d4d353ca6a689130f9003c600c0e47093
Rodolfo Gonzalez <rodogonzalez.cr@gmail.com>
2025-12-23 09:53:16 -0600

    cache use

--- resources/views/sections/carmodels.blade.php
+++ resources/views/sections/carmodels.blade.php
@@ -3,9 +3,11 @@
     <div class="box">
         <div class="box-img"><img src="/images/{{ $type->code  }}.png" alt="car image"></div>
         <h3>{{$type->name}}</h3>
-        <h2>${{$type->price}} por dia | ${{($type->price * 4 * 7)}} <span>/month</span></h2>
+        <h2>${{$type->price}} por dia <span><small>${{($type->price * 4 * 7)}} /mes</small></span></h2>
+        
         <a href="#" class="btn"  onclick="set_car('{{$type->code}}')">Reservar</a>
-        <small>** No incluye impuestos ni seguros</small>
+        <small>** No incluye impuestos ni seguros</small><br>
+        <small>** Tarifa Periodo : {{$type->season}}</small>
     </div>
     @endforeach
 </div>
